892bc50982be88e26535be3ffe6e5d463c60e791
Thoranin Chaiyakam <thoranin@trueaxiongames.com>
2021-01-23 20:58:45 +0700

    Road Node and Primitive

--- Assets/Script/RoadNode.cs
+++ Assets/Script/RoadNode.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoadNode : MonoBehaviour
 {
+    public event Action obstructionCleared;
+
     [SerializeField]
     private RoadNode[] forwardNodes = default;
+
+    private Vehicle[] entryQue = default;
 
     // Start is called before the first frame update
     void Start()
