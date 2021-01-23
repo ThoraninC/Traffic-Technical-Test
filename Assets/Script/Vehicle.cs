abea950b42bdc94057cf2cdf4f25aed9db5a65e2
Thoranin Chaiyakam <thoranin@trueaxiongames.com>
2021-01-23 21:08:10 +0700

    More Primitive

---
+++ Assets/Script/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField]
+    private RoadNode[] wayPoints = default;
+
+    private RoadNode headingNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HeadingTo(RoadNode node)
+    {
+        headingNode = node;
+    }
+}
